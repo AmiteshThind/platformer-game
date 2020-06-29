2a64eb9c674f57683f4ff161d4c6bbbf00b6153f
Amitesh Thind <thindamitesh@gmail.com>
2020-06-28 23:24:22 -0400

    Added Player Dash Ability
    Use Left shift to Dash and Z for jump

--- PlatformerGame/Assets/Scenes/Scripts/Player/PlayerDash.cs
+++ PlatformerGame/Assets/Scenes/Scripts/Player/PlayerDash.cs
@@ -4,15 +4,85 @@ using UnityEngine;
 
 public class PlayerDash : MonoBehaviour
 {
+    public bool DashCharged = true; 
+    public float DashForce = 1f;
+    public float StartDashTimer = 0.15f;
+    float CurrentDashTimer;
+    float DashDirection; 
+    private float moveKeyBoardInput,moveJoyStickInput;
+    public bool isDashing;
+    public float DashRechargeTime =5f; 
+    Joystick joystick;
+    Rigidbody2D rb;
+    DashButton dashbutton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        joystick = FindObjectOfType<Joystick>();
+        dashbutton = FindObjectOfType<DashButton>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         
+        moveKeyBoardInput = Input.GetAxis("Horizontal");
+        moveJoyStickInput = joystick.Horizontal;
+
+        if ((moveKeyBoardInput >0 || moveJoyStickInput > 0))
+        {
+            DashDirection = 1;
+        }
+        else if ((moveKeyBoardInput < 0 || moveJoyStickInput < 0))
+        {
+            DashDirection = -1;
+        }
+        else
+        {
+            DashDirection = 0; 
+        }
+      
+     
     }
-}
+
+    void FixedUpdate()
+    {
+
+        if (!DashCharged)
+        {
+            DashRechargeTime -= Time.deltaTime;
+            if (DashRechargeTime <= 0f)
+            {
+                DashRechargeTime = 5f;
+                DashCharged = true;
+            }
+        }
+
+
+        if ((Input.GetKeyDown(KeyCode.LeftShift) || dashbutton.Pressed) && DashCharged)
+        {
+            isDashing = true;
+            DashCharged = false;
+            CurrentDashTimer = StartDashTimer;
+            rb.velocity = Vector2.zero;
+            DashDirection = (int)moveKeyBoardInput; // Change To joystick if using joystick 
+        }
+
+        if (isDashing)
+        {
+            this.transform.position = new Vector2(this.transform.position.x + DashForce*DashDirection, this.transform.position.y);
+            CurrentDashTimer -= Time.deltaTime;
+        }
+
+        if (CurrentDashTimer <= 0)
+        {
+            isDashing = false;
+           
+        }
+
+    }
+
+   }
+ 
---
+++ PlatformerGame/Assets/Scenes/Scripts/UI/DashButton.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DashButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool Pressed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    // Event Handlers for Button Press
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+        
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+       
+    }
+}
