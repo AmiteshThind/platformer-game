eaeb17c2ec43d408095fa19bd54352bf86844b4b
Amitesh Thind <thindamitesh@gmail.com>
2020-07-14 02:27:12 -0400

    Added Element 1 Level 1 Platform

---
+++ PlatformerGame/Assets/Scenes/Fire Level/Levels/Level 1/CoinRotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int rotateSpeed; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            //or gameObject.SetActive(false);
+        }
+    }
+
+}
