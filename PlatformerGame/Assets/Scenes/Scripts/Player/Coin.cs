7700a6b78091c0e557c641bec8a84696bbb495d4
Amitesh Thind <thindamitesh@gmail.com>
2020-07-30 18:10:43 -0400

    adeed coins for level 1 and 2

---
+++ PlatformerGame/Assets/Scenes/Scripts/Player/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int coinCount = 0; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            coinCount++;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PlatformerGame/Assets/SkyLightingChangeEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyLightingChangeEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEngine.Experimental.Rendering.Universal.Light2D light2d;
+    public float counter =0f;
+    public float duration = 300f;
+    public float InitialIntensity;
+    public float FinalIntensity; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        light2d.intensity = Mathf.Lerp(InitialIntensity,FinalIntensity, counter / duration);
+    }
+}
