593fbdc97db2d6d1467ee02769711e0da74fe3e7
Amitesh Thind <thindamitesh@gmail.com>
2020-08-18 14:20:03 -0400

    added level 7

---
+++ PlatformerGame/Assets/YCG_PendulumKit/Pendulum.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pendulum : MonoBehaviour
+{
+    #region Public Variables
+    public Rigidbody2D body2d;
+    public float leftPushRange;
+    public float rightPushRange;
+    public float velocityThreshold;
+    #endregion //Public Variables
+
+    #region Private Variables
+
+    #endregion //Private Variables
+
+    // (Unity Named Methods)
+    #region Main Methods
+    void Start()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+        body2d.angularVelocity = velocityThreshold;
+    }
+    //Update is called by Unity every frame
+    void Update()
+    {
+        Push();
+    }
+    #endregion //Main Methods
+
+    //(Custom Named Methods)
+    #region Utility Methods 
+    public void Push()
+    {
+        if (transform.rotation.z > 0
+            && transform.rotation.z < rightPushRange
+            && (body2d.angularVelocity > 0)
+            && body2d.angularVelocity < velocityThreshold)
+        {
+            body2d.angularVelocity = velocityThreshold;
+        }
+        else if (transform.rotation.z < 0
+            && transform.rotation.z > leftPushRange
+            && (body2d.angularVelocity < 0)
+            && body2d.angularVelocity > velocityThreshold * -1)
+        {
+            body2d.angularVelocity = velocityThreshold * -1;
+        }
+
+    }
+    #endregion //Utility Methods
+
+    //Coroutines run parallel to other fucntions
+    #region Coroutines
+
+    #endregion //Coroutines
+}
